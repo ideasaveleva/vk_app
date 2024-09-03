1e4050955af5aaae637bd3a00a60bddf22984657
Ekaterina Saveleva <Ash2789@mail.ru>
2024-09-03 11:17:33 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore, Timestamp } from 'firebase/firestore'
 
 // Ваши настройки Firebase
 const firebaseConfig = {
-  apiKey: 'AIzaSyAfjiSU_aFX8eCWnZN6_-Ev3Zv7sNKMFPU',
-  authDomain: 'vk-my-store.firebaseapp.com',
-  projectId: 'vk-my-store',
-  storageBucket: 'vk-my-store.appspot.com',
-  messagingSenderId: '647386621302',
-  appId: '1:647386621302:web:645bb3d84068e5e3788657',
-  measurementId: 'G-X9Y762THVJ',
+  apiKey: 'настройки из Firebase',
+  authDomain: 'настройки из Firebase',
+  projectId: 'настройки из Firebase',
+  storageBucket: 'настройки из Firebase',
+  messagingSenderId: 'настройки из Firebase',
+  appId: 'настройки из Firebase',
+  measurementId: 'настройки из Firebase',
 }
 
 // Инициализация Firebase
